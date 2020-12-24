e328e4b23b08850f4076c9464af1bffc4f6a89cb
Straight <gengzhi1993@163.com>
2020-12-24 20:36:51 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <straight />
+</template>
+
+<script>
+import Straight from "./components/Straight.vue";
+
+export default {
+  name: "App",
+  components: {
+    Straight,
+  },
+};
+</script>
