cec2fa2e48b349cea25952202c61cf319c222f52
Straight <gengzhi1993@163.com>
2020-12-25 15:53:05 +0800

    封装Topnav组件

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,8 @@
 <template>
-  <div>
-    导航栏 | <router-link to="/">1</router-link> |
-    <router-link to="/xxx">2</router-link>
-  </div>
-  <hr />
   <router-view />
 </template>
 
 <script>
-import Straight from "./components/Straight.vue";
-
 export default {
   name: "App",
 };
--- src/components/Straight2.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div>团子</div>
-</template>
