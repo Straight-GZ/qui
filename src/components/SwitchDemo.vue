be7fb2fcb531f4e58f63aaef252ce6c835600a78
Straight <gengzhi1993@163.com>
2020-12-30 21:09:37 +0800

    可以接受value 和 input

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,7 +1,14 @@
 <template>
-  <div><Switch /></div>
+  <div><Switch :value="y" @input="y = $event" /></div>
 </template>
 <script lang="ts">
 import Switch from "../lib/Switch.vue";
-export default { components: { Switch } };
+import { ref } from "vue";
+export default {
+  components: { Switch },
+  setup() {
+    const y = ref(false);
+    return { y };
+  },
+};
 </script>
