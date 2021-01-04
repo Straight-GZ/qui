2ffda320b8c3af80ef7edbc0770edd92178cf03b
Straight <gengzhi1993@163.com>
2021-01-04 20:52:44 +0800

    创建 Button 组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,3 +1,11 @@
 <template>
   <div>button 内容</div>
+  <h1>示例</h1>
+  <Button>按钮</Button>
 </template>
+<script>
+import Button from "../lib/Button.vue";
+export default {
+  components: { Button },
+};
+</script>
