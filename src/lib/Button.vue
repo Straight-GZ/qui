5c238e22b91a0669638a237e1cb16dd700a4572d
Straight <gengzhi1993@163.com>
2021-01-05 20:19:07 +0800

    加载中 css

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,6 +1,6 @@
 <template>
   <button class="lunzi-button" :class="classes" :disabled="disabled">
-    <slot />
+    <span v-if="loading" class="lunzi-loadingIndicator"></span><slot />
   </button>
 </template>
 <script>
@@ -20,6 +20,10 @@ export default {
       default: "normal",
     },
     disabled: {
+      type: Boolean,
+      default: false,
+    },
+    loading: {
       type: Boolean,
       default: false,
     },
@@ -167,6 +171,25 @@ $radius: 4px;
       cursor: not-allowed;
       color: $grey;
     }
+  }
+  > .lunzi-loadingIndicator {
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px;
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: lunzi-spin 1s infinite linear;
+  }
+}
+@keyframes lunzi-spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
   }
 }
 </style>
