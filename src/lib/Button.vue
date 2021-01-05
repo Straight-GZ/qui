86a4f38021347b101bada021db3005406742c0e8
Straight <gengzhi1993@163.com>
2021-01-05 20:10:24 +0800

    支持disabled

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="lunzi-button" :class="classes">
+  <button class="lunzi-button" :class="classes" :disabled="disabled">
     <slot />
   </button>
 </template>
@@ -15,13 +15,22 @@ export default {
       type: String,
       default: "normal",
     },
+    level: {
+      type: String,
+      default: "normal",
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
   },
   setup(props) {
-    const { theme, size } = props;
+    const { theme, size, level } = props;
     const classes = computed(() => {
       return {
         [`lunzi-theme-${theme}`]: theme,
         [`lunzi-size-${size}`]: size,
+        [`lunzi-level-${level}`]: level,
       };
     });
     return { classes };
@@ -32,7 +41,9 @@ export default {
 $h: 32px;
 $border-color: #d9d9d9;
 $color: #333;
-$blue: #40a9ff;
+$red: #f56c6c;
+$blue: #67c23a;
+$grey: #d2e9ff;
 $radius: 4px;
 .lunzi-button {
   box-sizing: border-box;
@@ -48,6 +59,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
   & + & {
     margin-left: 8px;
   }
@@ -80,16 +92,80 @@ $radius: 4px;
       background: darken(white, 5%);
     }
   }
+
+  &.lunzi-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+  &.lunzi-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
   &.lunzi-theme-button {
-    &.lunzi-size-big {
-      font-size: 24px;
-      height: 48px;
-      padding: 0 16px;
+    &.lunzi-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
     }
-    &.lunzi-size-small {
-      font-size: 12px;
-      height: 20px;
-      padding: 0 4px;
+    &.lunzi-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  &.lunzi-theme-link {
+    &.lunzi-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.lunzi-theme-text {
+    &.lunzi-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.lunzi-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.lunzi-theme-button {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+      background: #c8c9cc;
+      &:hover {
+        border-color: $grey;
+      }
+    }
+  }
+  &.lunzi-theme-link,
+  &.lunzi-theme-text {
+    &[disabled] {
+      background: #c8c9cc;
+      cursor: not-allowed;
+      color: $grey;
     }
   }
 }
