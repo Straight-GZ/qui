980a95afaf11dd79754ae4a20ef1856cbe8503c1
Straight <gengzhi1993@163.com>
2021-01-06 14:16:18 +0800

    支持  点击关闭

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,11 +1,34 @@
 <template>
   <div>dialog 内容</div>
   <h1>示例1</h1>
-  <Dialog></Dialog>
+  <Button @click="toggle">toggle</Button>
+  <Dialog
+    v-model:visible="x"
+    :onClickOverlay="false"
+    :ok="f1"
+    :cancel="f2"
+  ></Dialog>
+  <!-- 等价于 <Dialog :visible="x" @update:visible="x = $event"></Dialog> -->
 </template>
 <script lang="ts">
+import { ref } from "vue";
 import Dialog from "../lib/Dialog.vue";
+import Button from "../lib/Button.vue";
 export default {
-  components: { Dialog },
+  components: { Dialog, Button },
+  setup() {
+    const x = ref(false);
+    const toggle = () => {
+      x.value = !x.value;
+    };
+    const f1 = () => {
+      console.log("1");
+      return false;
+    };
+    const f2 = () => {
+      console.log("2");
+    };
+    return { x, toggle, f1, f2 };
+  },
 };
 </script>
