337168ec8aa6960a2bee07f769cd624295e02428
Straight <gengzhi1993@163.com>
2021-01-07 22:43:15 +0800

    实现下划线 滑动

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="lunzi-tabs">
-    <div class="lunzi-tabs-nav">
+    <div class="lunzi-tabs-nav" ref="container">
       <div
         class="lunzi-tabs-nav-item"
+        :ref="
+          (el) => {
+            if (el) navItems[index] = el;
+          }
+        "
         @click="select(t)"
         :class="{ selected: t === selected }"
         v-for="(t, index) in titles"
@@ -10,6 +15,7 @@
       >
         {{ t }}
       </div>
+      <div class="lunzi-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="lunzi-tabs-content">
       <component
@@ -23,7 +29,7 @@
   </div>
 </template>
 <script lang="ts">
-import { computed } from "vue";
+import { computed, onMounted, onUpdated, ref } from "vue";
 import Tab from "./Tab.vue";
 export default {
   props: {
@@ -32,6 +38,24 @@ export default {
     },
   },
   setup(props, context) {
+    const navItems = ref<HTMLDivElement[]>([]);
+    const indicator = ref<HTMLDivElement>(null);
+    const container = ref<HTMLDivElement>(null);
+    const x = () => {
+      const divs = navItems.value;
+      const result = divs.filter((div) =>
+        div.classList.contains("selected")
+      )[0];
+      console.log(result);
+      const { width } = result.getBoundingClientRect();
+      indicator.value.style.width = width + "px";
+      const { left: left1 } = container.value.getBoundingClientRect();
+      const { left: left2 } = result.getBoundingClientRect();
+      const left = left2 - left1;
+      indicator.value.style.left = left + "px";
+    };
+    onMounted(x);
+    onUpdated(x);
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
@@ -49,7 +73,15 @@ export default {
     const select = (title) => {
       context.emit("update:selected", title);
     };
-    return { defaults, titles, current, select };
+    return {
+      defaults,
+      titles,
+      current,
+      select,
+      navItems,
+      indicator,
+      container,
+    };
   },
 };
 </script>
@@ -62,6 +94,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
     &-item {
       padding: 8px 0;
       margin: 0 16px;
@@ -72,6 +105,15 @@ $border-color: #d9d9d9;
       &.selected {
         color: $blue;
       }
+    }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms;
     }
   }
   &-content {
