e79fdad39f5b64512917a8609595c1b6bb225d40
Straight <gengzhi1993@163.com>
2021-01-11 20:28:38 +0800

    改造 Dialog

---
+++ src/components/Dialog2Demo.vue
@@ -0,0 +1,29 @@
+<demo>一键打开 Dialog</demo>
+<template>
+  <div>
+    <Button @click="showDialog">点击打开</Button>
+  </div>
+</template>
+<script lang="ts">
+import Button from "../lib/Button.vue";
+import { openDialog } from "../lib/openDialog";
+export default {
+  components: { Button },
+  setup() {
+    const showDialog = () => {
+      openDialog({
+        title: "标题",
+        content: "内容",
+        closeOnClickOverlay: false,
+        ok() {
+          console.log("ok");
+        },
+        cancel() {
+          console.log("cancel");
+        },
+      });
+    };
+    return { showDialog };
+  },
+};
+</script>
--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,55 +1,15 @@
 <template>
-  <div>dialog 内容</div>
-  <h1>示例1</h1>
-  <Button @click="toggle">toggle</Button>
-  <Dialog v-model:visible="x" :closeOnClickOverlay="true" :ok="f1" :cancel="f2">
-    <template v-slot:content>
-      <strong>hi</strong>
-      <div>你好</div>
-    </template>
-    <template v-slot:title>
-      <strong>加粗的标签 </strong>
-    </template>
-  </Dialog>
-  <h1>示例2</h1>
-  <div>
-    <Button @click="showDialog">show</Button>
-  </div>
-  <!-- 等价于 <Dialog :visible="x" @update:visible="x = $event"></Dialog> -->
+  <Demo :component="Dialog1Demo" />
+  <Demo :component="Dialog2Demo" />
 </template>
 <script lang="ts">
-import Dialog from "../lib/Dialog.vue";
-import Button from "../lib/Button.vue";
-import { ref } from "vue";
-import { openDialog } from "../lib/openDialog";
+import Demo from "./Demo.vue";
+import Dialog1Demo from "./Dialog1Demo.vue";
+import Dialog2Demo from "./Dialog2Demo.vue";
 export default {
-  components: { Dialog, Button },
+  components: { Demo },
   setup() {
-    const x = ref(false);
-    const toggle = () => {
-      x.value = !x.value;
-    };
-    const f1 = () => {
-      console.log("1");
-      return false;
-    };
-    const f2 = () => {
-      console.log("2");
-    };
-    const showDialog = () => {
-      openDialog({
-        title: "标题",
-        content: "内容",
-        closeOnClickOverlay: false,
-        ok() {
-          console.log("ok");
-        },
-        cancel() {
-          console.log("cancel");
-        },
-      });
-    };
-    return { x, toggle, f1, f2, showDialog };
+    return { Dialog1Demo, Dialog2Demo };
   },
 };
 </script>
